c30b2da38103b9ecf58bdb230e995d28ec08b86e
KhangDZ <loveleesn@gmail.com>
2022-03-16 10:04:14 +0700

    prettier lint-staged husky

--- src/index.js
+++ src/index.js
@@ -1,31 +1,30 @@
-const { engine, create } = require('express-handlebars')
-const express = require('express')
-const path = require('path')
-const morgan = require('morgan')
-const app = express()
-const port = 3000
+const { engine, create } = require('express-handlebars');
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const app = express();
+const port = 3000;
 
 // Static files
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
 // HTTP middleware
-app.use(morgan('combined'))
+app.use(morgan('combined'));
 
 // .hbs
 const hbs = create({
-  extname: 'hbs'
-})
+    extname: 'hbs',
+});
 
 // template engine
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
-app.set('views',__dirname + '\\resources\\views');
-
+app.set('views', __dirname + '\\resources\\views');
 
 app.get('/', (req, res) => {
-  return res.render('home')
-})
+    return res.render('home');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
